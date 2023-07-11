4b7479cfc65ad3dede98ada569dc0548459ec2c3
Samuel Matos <samuhmatos@gmail.com>
2023-07-11 10:52:00 -0300

    Bem vindo ao React Native: Rodando o App - View e Text (Core Components)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ViewStyle } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          lineHeight: 40,
+          backgroundColor: "red",
+          fontSize: 25,
+          textAlign: "center",
+        }}
+      >
+        Open up App.js to start working on your app!
+      </Text>
+      <Text> Profissional React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
