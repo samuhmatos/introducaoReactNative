2165c588f3892fb8adc503b8061a8ad939bcc1c7
Samuel Matos <samuhmatos@gmail.com>
2023-07-14 15:58:06 -0300

    Layout com FlexBox: Flex e Flex Direction

--- App.tsx
+++ App.tsx
@@ -3,10 +3,13 @@ import { StyleSheet, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{ width: 100, height: 100, backgroundColor: "tomato" }} />
-      <View style={{ width: 100, height: 100, backgroundColor: "gold" }} />
+      <View style={{ backgroundColor: "tomato", flex: 2 }} />
+      <View style={{ backgroundColor: "gold", flex: 2 }} />
       <View
-        style={{ width: 100, height: 100, backgroundColor: "dodgerblue" }}
+        style={{
+          backgroundColor: "dodgerblue",
+          flex: 2,
+        }}
       />
     </View>
   );
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#eeee",
     flex: 1,
+    flexDirection: "column",
   },
 });
