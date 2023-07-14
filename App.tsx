76160e2e4f211aa3296209f0bb576673b85ca18a
Samuel Matos <samuhmatos@gmail.com>
2023-07-14 15:59:37 -0300

    Layout com FlexBox: Justify Content - Alinhamento no Eixo Principal

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { StyleSheet, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{ backgroundColor: "tomato", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 2 }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "tomato" }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "gold" }} />
       <View
         style={{
+          width: 100,
+          height: 100,
           backgroundColor: "dodgerblue",
-          flex: 2,
         }}
       />
     </View>
@@ -19,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#eeee",
     flex: 1,
-    flexDirection: "column",
+    //flexDirection: "column",
+    justifyContent: "space-around",
   },
 });
